f1d9bd745b14775326ca1c3fc71f663040575e52
micheaol <micheaol80@gmail.com>
2022-01-25 06:01:44 +0000

    Fix linter error

--- person.rb
+++ person.rb
@@ -1,58 +1,57 @@
+# frozen_string_literal: true
+
 class Person
-      attr_writer :id
-      attr_accessor :name
-    attr_accessor :age
-    
-    def initialize (age, name = "Unknown", parent_permission = true)
-        @id = Random.rand(10...100)
-        @name = name
-        @age = age
-    end
+  attr_writer :id
+  attr_accessor :name, :age
 
-    def can_use_services?
-        if @age >= 18 || @parent_permission == true
-            return true
-        else
-            return false
-        end
-    end
+  def initialize(age, name = 'Unknown', _parent_permission = true)
+    @id = Random.rand(10...100)
+    @name = name
+    @age = age
+  end
 
-    def say_age
-        print @age
+  def can_use_services?
+    if @age >= 18 || @parent_permission == true
+      true
+    else
+      false
     end
+  end
 
-    private
-    def is_of_age?
-        if @age >= 18
-            return true
-        else
-            return false
-        end
-    end
+  def say_age
+    print @age
+  end
+
+  private
+
+  def is_of_age?
+    @age >= 18
+  end
 end
 
 class Student < Person
-    def initialize(classroom, age, name = "Unknown", parent_permission = true)
-     super(age, name = "Unknown", parent_permission = true)
+  def initialize(classroom, age, _name = 'Unknown', _parent_permission = true)
+    super(age, name = 'Unknown', parent_permission = true)
     @classroom = classroom
-    end
-    
-    def play_hooky
-        return "¯\(ツ)/¯"
-    end
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
 
 class Teacher < Person
-    def initialize(specialization, age, name = "Unknown", parent_permission = true)
-super(age, name = "Unknown", parent_permission = true)
-    @specialization = specialization 
-    end
-    def can_use_services?
-         return true
-    end
+  def initialize(specialization, age, _name = 'Unknown', _parent_permission = true)
+    super(age, name = 'Unknown', parent_permission = true)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
 
-teacher = Teacher.new(25, "class");
-student = Student.new("Jenny", 10)
+teacher = Teacher.new(25, 'class')
+student = Student.new('Jenny', 10)
 pust student.can_use_services?
 puts teacher.can_use_services?
