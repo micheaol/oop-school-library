76cc1af981b327ac2f9855a9c4fcd7c269c1cacf
micheaol <micheaol80@gmail.com>
2022-01-26 21:23:02 +0000

    Initialized rental

--- rental.rb
+++ rental.rb
@@ -5,7 +5,11 @@ class Rental
 
   def initialize(date, person, book)
     @date = date
+
     @book = book
+    book.rentals << self unless book.rentals.include?(self)
+
     @person = person
+    person.rentals << self unless person.rentals.include?(self)
   end
 end
