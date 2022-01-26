62037387077a3052664a7b434b93645db5cd20f6
micheaol <micheaol80@gmail.com>
2022-01-26 21:43:56 +0000

    Fix linter error

--- student.rb
+++ student.rb
@@ -10,4 +10,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.student << self unless classroom.student.include?(self)
+  end
 end
