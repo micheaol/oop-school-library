af2540fd9040451b96f733f09bc9ba1c48895251
micheaol <micheaol80@gmail.com>
2022-01-26 21:18:46 +0000

    Add method to add student to classroom

--- classroom.rb
+++ classroom.rb
@@ -4,5 +4,11 @@ class Classroom
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_students(student)
+    students << student
+    student.classroom = self
   end
 end
