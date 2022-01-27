dc2f0fa02be090a0f9ad113f91d37fc7cf22b758
micheaol <micheaol80@gmail.com>
2022-01-27 21:22:55 +0000

    Fix linter error

--- app.rb
+++ app.rb
@@ -147,12 +147,12 @@ class App
 
   def list_rental_by_person_id
     print "Person's ID: "
-    p_id = gets.chomp.to_i
+    person_id = gets.chomp.to_i
 
     puts 'Rentals: '
 
     @rentals.each do |rental|
-      puts "Date: #{rental.date} Book: #{rental.book.title} by: #{rental.book.author} if rental.person.id == p_id"
+      puts "Date: #{rental.date} Book: #{rental.book.title} by: #{rental.book.author}" if rental.person.id == person_id
     end
 
     user_choice
