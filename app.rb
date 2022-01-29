4ee63d8cec91a13108dbbd1ad79d6a890bf62f0e
micheaol <micheaol80@gmail.com>
2022-01-29 17:11:12 +0100

    Unable to display option based of the menu selected

--- app.rb
+++ app.rb
@@ -22,13 +22,13 @@ class App
   def user_choice
     puts ' '
     puts 'Please choose an option by entering a number: '
-    puts '1 - List of books'
-    puts '2 - List of People'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+    puts '1- List of books'
+    puts '2- List of People'
+    puts '3- Create a person'
+    puts '4- Create a book'
+    puts '5- Create a rental'
+    puts '6- List all rentals for a given person id'
+    puts '7- Exit'
     user_selection = gets.chomp
     selected_choice user_selection
   end
